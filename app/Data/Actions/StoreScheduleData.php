361faa870f00ad4e9f851eb48fb2c41d0b0275b7
Guilherme Ferreti <guiferretis@gmail.com>
2023-11-17 15:17:01 -0300

    Added validation to store schedule endpoint

--- app/Data/Actions/StoreScheduleData.php
+++ app/Data/Actions/StoreScheduleData.php
@@ -6,16 +6,27 @@ namespace App\Data\Actions;
 
 use Carbon\Carbon;
 use Spatie\LaravelData\Attributes\MapInputName;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Mappers\CamelCaseMapper;
 
 #[MapInputName(CamelCaseMapper::class)]
 class StoreScheduleData extends Data
 {
-    public function __construct(
-        public string $customer_phone_number,
-        public string $customer_name,
-        public Carbon $scheduled_to,
-    ) {
+    public string $customer_phone_number;
+
+    public string $customer_name;
+
+    #[WithCast(DateTimeInterfaceCast::class, format: 'Y-m-d H:i')]
+    public Carbon $scheduled_to;
+
+    public static function rules(): array
+    {
+        return [
+            'customer_phone_number' => ['required', 'string', 'between:12,13'],
+            'customer_name'         => ['required', 'string', 'max:255'],
+            'scheduled_to'          => ['required', 'date_format:Y-m-d H:i'],
+        ];
     }
 }

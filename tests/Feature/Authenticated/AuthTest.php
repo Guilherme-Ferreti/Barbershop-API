b2740875a9ed572df8dc59754e8f28fe68686cc3
Guilherme Ferreti <guiferretis@gmail.com>
2023-11-29 10:30:00 -0300

    Moved profiles tests to separate class

--- tests/Feature/Authenticated/AuthTest.php
+++ tests/Feature/Authenticated/AuthTest.php
@@ -2,13 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Domain\Authenticated\Actions\Login;
-use App\Domain\Authenticated\Data\LoginData;
 use App\Domain\Common\Models\Customer;
 
-use function Pest\Laravel\actingAs;
-use function Pest\Laravel\assertDatabaseHas;
-use function Pest\Laravel\getJson;
 use function Pest\Laravel\postJson;
 
 uses()->group('authenticated');
@@ -46,32 +41,4 @@ test('a customer cannot login using non-existing phone number', function () {
     ];
 
     postJson(route('authenticated.login'), $payload)->assertUnprocessable();
-});
-
-test('jwt authentication works', function () {
-    $customer = Customer::factory()->create();
-
-    [, $jwt] = app(Login::class)->handle(LoginData::from(['phoneNumber' => $customer->phone_number]));
-
-    $route = route('authenticated.profile.show');
-
-    getJson($route)->assertUnauthorized();
-
-    getJson($route, ['Authorization' => "Bearer $jwt"])->assertOk();
-});
-
-test('a customer can update it\'s profile', function () {
-    $customer = Customer::factory()->create();
-
-    $payload = [
-        'name' => fake()->name(),
-    ];
-
-    $route = route('authenticated.profile.update');
-
-    $this->assertAuthenticatedOnly($route, 'patch');
-
-    actingAs($customer)->patchJson($route, $payload)->assertOk();
-
-    assertDatabaseHas(Customer::class, $payload);
 });
---
+++ tests/Feature/Authenticated/ProfileTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Domain\Authenticated\Data\LoginData;
+use App\Domain\Common\Models\Customer;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\getJson;
+
+uses()->group('authenticated');
+
+test('jwt authentication works', function () {
+    $customer = Customer::factory()->create();
+
+    [, $jwt] = app(Login::class)->handle(LoginData::from(['phoneNumber' => $customer->phone_number]));
+
+    $route = route('authenticated.profile.show');
+
+    getJson($route)->assertUnauthorized();
+
+    getJson($route, ['Authorization' => "Bearer $jwt"])->assertOk();
+});
+
+test('a customer can update it\'s profile', function () {
+    $customer = Customer::factory()->create();
+
+    $payload = [
+        'name' => fake()->name(),
+    ];
+
+    $route = route('authenticated.profile.update');
+
+    $this->assertAuthenticatedOnly($route, 'patch');
+
+    actingAs($customer)->patchJson($route, $payload)->assertOk();
+
+    assertDatabaseHas(Customer::class, $payload);
+});

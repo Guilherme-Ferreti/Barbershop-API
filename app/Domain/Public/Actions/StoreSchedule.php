af48fd7983ac1ffa0b776d13ffc3bf4b5db3a8be
Guilherme Ferreti <guiferretis@gmail.com>
2023-12-18 14:59:52 -0300

    Added database transaction when storing schedule

--- app/Domain/Public/Actions/StoreSchedule.php
+++ app/Domain/Public/Actions/StoreSchedule.php
@@ -7,21 +7,24 @@ namespace App\Domain\Public\Actions;
 use App\Domain\Common\Models\Customer;
 use App\Domain\Common\Models\Schedule;
 use App\Domain\Public\Data\Actions\StoreScheduleData;
+use Illuminate\Support\Facades\DB;
 
 class StoreSchedule
 {
     public function handle(StoreScheduleData $data): Schedule
     {
-        $customer = Customer::firstOrCreate(
-            ['phone_number' => $data->customer_phone_number],
-            ['name' => $data->customer_name],
-        );
-
-        $customer->pendingSchedule()->delete();
-
-        return $customer->schedules()->create([
-            'scheduled_to'  => $data->scheduled_to,
-            'customer_name' => $data->customer_name,
-        ]);
+        return DB::transaction(function () use ($data) {
+            $customer = Customer::firstOrCreate(
+                ['phone_number' => $data->customer_phone_number],
+                ['name' => $data->customer_name],
+            );
+    
+            $customer->pendingSchedule()->delete();
+    
+            return $customer->schedules()->create([
+                'scheduled_to'  => $data->scheduled_to,
+                'customer_name' => $data->customer_name,
+            ]);
+        });
     }
 }
